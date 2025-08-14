00b918660984f2671e1de4ebdf573895e665ed19
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-08-14 17:33:31 +0900

    Feat: dto 수정

--- src/main/java/edu/sookmyung/talktitude/chat/dto/ClientChatSessionDto.java
+++ src/main/java/edu/sookmyung/talktitude/chat/dto/ClientChatSessionDto.java
@@ -11,7 +11,6 @@ public class ClientChatSessionDto {
     private String status;         // "IN_PROGRESS" | "FINISHED"
     private String storeName;
     private String storeImageUrl;
-    private String orderSummary;   // 예) "아기만두 외 3개" / "주문 외 문의"
-    private int totalPrice;        // 없으면 0
+    private String OrderSummary;  // 가격 포함
     private String lastMessage;    // 클라이언트 관점 textToShow
 }

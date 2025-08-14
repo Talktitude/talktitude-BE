046106555fe6a5d9f2039cd4dac78ef60c5f995c
mhee167 <mhee116677@gmail.com>
2025-08-14 16:39:12 +0900

    refactor : API 명세 맞추기 위해 ErrorCode 수정

--- src/main/java/edu/sookmyung/talktitude/common/exception/ErrorCode.java
+++ src/main/java/edu/sookmyung/talktitude/common/exception/ErrorCode.java
@@ -39,9 +39,10 @@ public enum ErrorCode {
     // Order 관련
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O01", "주문을 찾을 수 없습니다."),
     ORDER_DELIVERY_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O02", "배달 정보를 찾을 수 없습니다."),
-    ORDER_MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O03", "메뉴 정보를 찾을 수 없습니다."),
-    ORDER_PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O04", "결제 정보를 찾을 수 없습니다."),
-    ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ORDER_O05", "해당 주문에 접근할 권한이 없습니다."),
+    ORDER_PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O03", "결제 정보를 찾을 수 없습니다."),
+    ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ORDER_O04", "해당 주문에 접근할 권한이 없습니다."),
+    ORDER_MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_O05", "메뉴 정보를 찾을 수 없습니다."),
+
 
     //Report 관련
     REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "REPORT_001", "리포트 정보를 찾을 수 없습니다."),

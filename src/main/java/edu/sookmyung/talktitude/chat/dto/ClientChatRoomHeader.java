87042ba341c4ecb70713e9be0f1b100ebc3b72bd
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-06 16:47:19 +0900

    Feat: dto에 status 추가

--- src/main/java/edu/sookmyung/talktitude/chat/dto/ClientChatRoomHeader.java
+++ src/main/java/edu/sookmyung/talktitude/chat/dto/ClientChatRoomHeader.java
@@ -1,5 +1,6 @@
 package edu.sookmyung.talktitude.chat.dto;
 
+import edu.sookmyung.talktitude.chat.model.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +11,5 @@ public class ClientChatRoomHeader {
     private Long orderId; // 주문 연결 없으면 null
     private String title; // 가게명 또는 "주문 외 문의"
     private boolean orderLinked; // 주문 연결 여부
+    private Status status;
 }

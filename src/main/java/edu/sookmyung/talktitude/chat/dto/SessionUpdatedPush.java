cf2b08dc4f10ef88702590b9a6868d7ac0e58091
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-10 20:37:33 +0900

    feat: SessionUpdatedPush 생성

---
+++ src/main/java/edu/sookmyung/talktitude/chat/dto/SessionUpdatedPush.java
@@ -0,0 +1,21 @@
+package edu.sookmyung.talktitude.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SessionUpdatedPush {
+    private Long sessionId;
+    private String clientLoginId;
+    private String clientPhone;
+    private String profileImageUrl;
+    private String status;
+    private LocalDateTime lastMessageTime;
+}
